372af60a24adaba0874a5fb11e735e92c70eeef0
calvindemon1 <calvin.leo0818@gmail.com>
2026-04-09 17:45:13 +0700

    FEAT : changes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin(), tailwindcss()],
   server: {
+    host: true,
     port: 3344,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
